96f921fc433e86405a8d7d41795b197f6f6a1518
Sweety Nandi <sweetynandi06062002@gmail.com>
2024-12-09 19:01:07 +0530

    changes in vercel.json file

--- src/index.js
+++ src/index.js
@@ -9,10 +9,12 @@ import orderRoutes from "./routes/orderRoutes.js";
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
   });
-// Routes
+
+  
 app.use('/api/auth', authRoutes);
 app.use('/api/menu', menuRoutes);
 app.use('/api/order', orderRoutes);
